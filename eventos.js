9e74169de086f8380d44fa1180b4fde7bdffde40
gonzalolacueva <lacuevagonzalo24@gmail.com>
2023-08-12 23:26:30 -0300

    Arreglo del formato

--- eventos.js
+++ eventos.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     const div = document.getElementById("verde");
-    const button = document.getElementById("button");
 
     div.addEventListener("click", function saludarDiv() {
         if (saludo === false) {
